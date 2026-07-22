a15c73474009753b4179ea57f98362d86706ec52
kuberan <kuberan1810@gmail.com>
2026-07-22 14:49:41 +0530

    index

--- src/app/index.tsx
+++ src/app/index.tsx
@@ -2,8 +2,6 @@ import { Redirect } from 'expo-router';
 import React from 'react';
 
 export default function Index() {
-
     // Redirect to the onboarding page initially
     return <Redirect href="/(instructor)/dashboard/dashboard" />;
-
 }
